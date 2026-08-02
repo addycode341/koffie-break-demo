ec34dfacf083f2f3668b5576b2949f2ee4ff46a7
addycode341 <adiparth342@gmail.com>
2026-08-03 01:09:10 +0530

    Koffie Break website complete

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import Navbar from "./components/koffie/Navbar";
+import Hero from "./components/koffie/Hero";
+import about from "./components/koffie/About";
+import Menu from "./components/koffie/Menu";
+import WhyChoose from "./components/koffie/WhyChoose";
+import Gallery from "./components/koffie/Gallery";
+import Reviews from "./components/koffie/Reviews";
+import Contact from "./components/koffie/Contact";
+import Footer from "./components/koffie/Footer";
+import WhatsAppButton from "./components/WhatsAppButton";
+
+
+function App(){
+
+const About = about;
+
+return(
+
+<>
+
+<Navbar/>
+
+<Hero/>
+
+<About/>
+
+<Menu/>
+
+<WhyChoose/>
+
+<Gallery/>
+
+<Reviews/>
+
+<Contact/>
+
+<Footer/>
+<WhatsAppButton/> 
+</>
+
+)
+
+}
+
+
+export default App;
---
+++ src/pages/KoffieBreakDemo.jsx
@@ -0,0 +1,42 @@
+import Navbar from "../components/koffie/Navbar";
+import Hero from "../components/koffie/Hero";
+import about from "../components/koffie/About";
+import Menu from "../components/koffie/Menu";
+import Gallery from "../components/koffie/Gallery";
+import WhyChoose from "../components/koffie/WhyChoose";
+import Reviews from "../components/koffie/Reviews";
+import Contact from "../components/koffie/Contact";
+import Footer from "../components/koffie/Footer";
+import WhatsAppButton from "../components/WhatsAppButton";
+
+function KoffieBreakDemo(){
+
+return(
+
+<div className="bg-[#0B0B0B] text-white overflow-hidden">
+
+<Navbar/>
+
+<Hero/>
+
+<about/>
+
+<Menu/>
+
+<Gallery/>
+
+<WhyChoose/>
+
+<Reviews/>
+
+< Contact/>
+
+<Footer/>
+<WhatsAppButton/>
+</div>
+
+)
+
+}
+
+export default KoffieBreakDemo;
